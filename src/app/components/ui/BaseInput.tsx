89cad690e3bb3521a7e4d096f34f1d39fa8b293d
ks-rushik <rushi.katrodiya@kalkani.in>
2025-02-21 16:41:28 +0530

    css changes in baseInput and baseLabel

--- src/app/components/ui/BaseInput.tsx
+++ src/app/components/ui/BaseInput.tsx
@@ -3,18 +3,36 @@ import {
   Input,
   InputProps,
   InputStylesNames,
+  PasswordInput,
+  PasswordInputProps,
 } from "@mantine/core";
 import clsx from "clsx";
 import { FC } from "react";
 
 type IBaseInputProps = ElementProps<"input", keyof InputProps> &
-  InputProps & {
-    classNames?: Partial<Record<InputStylesNames, string>>;
+  InputProps & PasswordInputProps & {
+    classNames?: Partial<Record<InputStylesNames, string>>
+  
   };
 
 const BaseInput: FC<IBaseInputProps> = (props) => {
-  const { classNames, disabled, ...other } = props;
+  const { classNames,type, disabled, ...other } = props;
   const { input, ...otherElement } = classNames || {};
+
+  if(type === 'password'){
+    return  <PasswordInput
+    classNames={{
+      ...otherElement,
+      input: clsx(
+        "bg-blue-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus-within:border-blue-500 block w-full p-2.5",
+        "dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+        input
+      ),
+    }}
+    disabled={disabled}
+    {...other}
+  />
+  }
 
   return (
     <Input

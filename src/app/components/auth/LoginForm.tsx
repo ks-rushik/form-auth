bd372dbd08bdaf444968b7953e1132dbe7468c8f
ks-rushik <rushi.katrodiya@kalkani.in>
2025-02-21 16:45:14 +0530

    loginform re-created

--- src/app/components/auth/LoginForm.tsx
+++ src/app/components/auth/LoginForm.tsx
@@ -6,17 +6,13 @@ import BaseButton from "../ui/BaseButton";
 import BaseInput from "../ui/BaseInput";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
 
 const loginSchema = z.object({
-  email: z.string().email("Invalid email format"),
-  password: z
-    .string()
-    .min(8, { message: 'Be at least 8 characters long' })
-    .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
-    .regex(/[0-9]/, { message: 'Contain at least one number.' })
-    .regex(/[^a-zA-Z0-9]/, {
-      message: 'Contain at least one special character.',
-    })
+  email: z.string().
+  min(1,"Email is Required").
+  email("Invalid email format"),
+  password: z.string().min(1, "Password required") 
 });
 
 type LoginFormData = z.infer<typeof loginSchema>;
@@ -35,31 +31,54 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="m-60">
-      <FormGroup>
-        <FormField label="Email" name="email" error={errors.email?.message}>
-          <BaseInput
-            {...register("email")}
-            placeholder="Enter your email..."
-            classNames={{ input: "w-52" }}
-          /> 
-        </FormField>
-        <FormField
-          label="Password"
-          name="password"
-          error={errors.password?.message}
-        >
-          <BaseInput
-            {...register("password")}
-            placeholder="Enter your Password"
-            classNames={{ input: "w-52" }}
-          />
-        </FormField>
-      </FormGroup>
-      <BaseButton type="submit" intent="success" classNames={{ root: "ml-4" }}>
-        Login
-      </BaseButton>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="">
+        <FormGroup>
+          <h1 className="flex flex-col items-center justify-center mb-10 text-xl">
+            Login Form
+          </h1>
+          <FormField label="Email" name="email" error={errors.email?.message}>
+            <BaseInput
+              {...register("email")}
+              type="email"
+              name="email"
+              placeholder="Enter your email..."
+            />
+          </FormField>
+          <FormField
+            label="Password"
+            name="password"
+            error={errors.password?.message}
+          >
+            <BaseInput
+              {...register("password")}
+              type="password"
+              name="password"
+              placeholder="Enter your Password"
+              classNames={{ input: "focus-within " }}
+            />
+          </FormField>
+          <BaseButton
+            type="submit"
+            intent="success"
+            classNames={{
+              root: "mb-2 w-full py-2 rounded-md",
+            }}
+          >
+            Login
+          </BaseButton>
+          <div className="flex justify-between text-md  mb-4">
+            <Link href="#">Forget password?</Link>
+            <div className="flex gap-1 justify-end ">
+              <span> Don't have account?</span>
+              <Link href="#" className="text-blue-600 hover:underline">
+                SignUp
+              </Link>
+            </div>
+          </div>
+        </FormGroup>
+      </form>
+    </>
   );
 };
 
